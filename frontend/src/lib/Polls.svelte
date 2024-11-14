fbf88b3dfd057281566dfb95f9cddda9d9176b20
598975 <89256888+598975@users.noreply.github.com>
2024-11-15 00:42:15 +0100

    fixes on poll creation and voting added

--- frontend/src/lib/Polls.svelte
+++ frontend/src/lib/Polls.svelte
@@ -2,6 +2,8 @@
     let question = "";
     let option_1 = "";
     let option_2 = "";
+    let getError = "";
+    let createError = "";
 
     async function createPoll() {
         try {
@@ -11,19 +13,19 @@
             const validUntil = newDate.toISOString();
             const options = [
                 {
-                    caption: option_1,
-                    presentationOrder: 1,
+                    "caption": option_1,
+                    "presentationOrder": 1,
                 },
                 {
-                    caption: option_2,
-                    presentationOrder: 2,
+                    "caption": option_2,
+                    "presentationOrder": 2,
                 },
             ];
             const data = {
                 question,
                 publishedAt,
                 validUntil,
-                options,
+                options
             };
             const res = await fetch("/api/Polls", {
                 method: "POST",
@@ -33,13 +35,13 @@
                 body: JSON.stringify(data),
             });
             if (!res.ok) {
+                createError = "Failed to create poll" + res.status;
                 throw new Error(`HTTP error! status: ${res.status}`);
             }
-            const result = await res.json();
-            return result;
+            window.location.reload();
         } catch (error) {
+            createError = "Failed to create poll";
             console.error("Failed to create poll:", error);
-            return [];
         }
     }
 
@@ -52,19 +54,39 @@
             const data = await res.json();
             return data;
         } catch (error) {
-            console.error("Failed to fetch polls:", error);
+            getError = "Failed to fetch polls";
             return [];
         }
     }
 
-    async function vote(pollId) {
-        console.log("Voting for poll:", pollId);
+    async function vote(pollId, optionId) {
+        try {
+            const data = {
+                pollId,
+                optionId,
+                publishedAt: new Date().toISOString(),
+            };
+            const res = await fetch("/api/Votes", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
+            });
+            if (!res.ok) {
+                throw new Error(`HTTP error! status: ${res.status}`);
+            }
+            alert("Vote successful");
+        } catch (error) {
+            console.error("Failed to vote:", error);
+        }
     }
 </script>
 
 <div class="w3-container w3-content" style="max-width: 800px;">
     <!-- <Link to="/register">Register</Link> -->
     <h1 class="w3-center">Polls</h1>
+    <p>{getError}</p>
     <ul class="w3-ul">
         {#await getPolls()}
             <p>Loading...</p>
@@ -78,7 +100,7 @@
                         <ul>
                             {#each poll.options as choice}
                                 <li>{choice.caption}</li>
-                                <form on:submit|preventDefault={() => vote(poll.id)}>
+                                <form on:submit|preventDefault={() => vote(poll.id, choice.id)}>
                                     <button type="submit" class="w3-button w3-blue">Vote</button>
                                 </form>
                             {/each}
@@ -91,6 +113,7 @@
         {/await}
     </ul>
     <h2 class="w3-center">Create Poll</h2>
+    <p>{createError}</p>
     <form on:submit|preventDefault={createPoll} class="w3-panel">
         <label for="question">Question</label>
         <input
